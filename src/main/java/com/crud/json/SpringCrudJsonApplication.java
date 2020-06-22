fe9ebbc4e021ba5774dfe8a82d5f115b0b309375
bhavesh357 <bkadam357@gmail.com>
2020-06-22 23:19:44 +0530

    first commit

---
+++ src/main/java/com/crud/json/SpringCrudJsonApplication.java
@@ -0,0 +1,13 @@
+package com.crud.json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudJsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCrudJsonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crud/json/SpringCrudJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudJsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
